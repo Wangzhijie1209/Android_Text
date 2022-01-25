022b54cf8290ebbea171e702c9ead3cd6f26e36a
Wangzhijie1209 <2201792896@qq.com>
2022-01-25 16:58:53 +0800

    intent打开电话本 打开网址

---
+++ app/src/main/java/com/example/test/intent/IntentActivity.java
@@ -0,0 +1,37 @@
+package com.example.test.intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.test.R;
+
+/**
+ *隐式Intent使用:
+ */
+public class IntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://www.baidu.com"));
+        startActivity(intent);
+    }
+
+    public void onclicktel(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:10086"));
+        startActivity(intent);
+    }
+
+    public void onClickgeo(View view) {
+    }
+}
